fa051bfbb59607905b5c5bd54e63e888379b29b1
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2024-06-11 15:14:25 +0900

    trim shell scripts under /tools
    1. Trimmed shell scripts under /tools.
    2. It's cosmetic change.
    
    Signed-off-by: Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>

--- tools/show-all.sh
+++ tools/show-all.sh
@@ -1,9 +1,9 @@
+#!/usr/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-BASE_DIR=${SCRIPT_DIR}/..
+BASE_DIR="${SCRIPT_DIR}"/..
 
-python ${BASE_DIR}/tools/show-euc-kr-doc.py ${BASE_DIR}/data/kiwoom-exported-transactions/year-2020-<YOUR_ACCOUNT_0000_HERE>.csv
-python ${BASE_DIR}/tools/show-euc-kr-doc.py ${BASE_DIR}/data/kiwoom-exported-transactions/year-2021-<YOUR_ACCOUNT_0000_HERE>.csv
-python ${BASE_DIR}/tools/show-euc-kr-doc.py ${BASE_DIR}/data/kiwoom-exported-transactions/year-2022-<YOUR_ACCOUNT_0000_HERE>.csv
-python ${BASE_DIR}/tools/show-euc-kr-doc.py ${BASE_DIR}/data/kiwoom-exported-transactions/year-2023-<YOUR_ACCOUNT_0000_HERE>.csv
-python ${BASE_DIR}/tools/show-euc-kr-doc.py ${BASE_DIR}/data/kiwoom-exported-transactions/year-2024-<YOUR_ACCOUNT_0000_HERE>.csv
-
+python "${BASE_DIR}"/tools/show-euc-kr-doc.py "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2020-5417-5299.csv
+python "${BASE_DIR}"/tools/show-euc-kr-doc.py "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2021-5417-5299.csv
+python "${BASE_DIR}"/tools/show-euc-kr-doc.py "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2022-5417-5299.csv
+python "${BASE_DIR}"/tools/show-euc-kr-doc.py "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-5417-5299.csv
+python "${BASE_DIR}"/tools/show-euc-kr-doc.py "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-5417-5299.csv
--- tools/unmask.sh
+++ tools/unmask.sh
@@ -1,5 +1,6 @@
+#!/usr/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR=${SCRIPT_DIR}/..
 
-python ${BASE_DIR}/tools/mask.py --config ${BASE_DIR}/config/mask.yaml --function=unmask
+python "${BASE_DIR}"/tools/mask.py --config "${BASE_DIR}"/config/mask.yaml --function=unmask
 
