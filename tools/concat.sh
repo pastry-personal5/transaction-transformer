006af71b55feb9b352bc14f4daf4a3db380759ad
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2024-07-08 03:27:45 +0900

    clean up kiwoom data after concat
    1. Now it cleans up Kiwoom data after concat.
    
    Signed-off-by: Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>

--- tools/concat.sh
+++ tools/concat.sh
@@ -2,18 +2,24 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 BASE_DIR="${SCRIPT_DIR}"/..
 
-# 5417-5299
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2020-5417-5299.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-5417-5299.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2021-5417-5299.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-5417-5299.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2022-5417-5299.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-5417-5299.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-5417-5299.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-5417-5299.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-5417-5299.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-5417-5299.csv
+# <YOUR_ACCOUNT_0000_HERE>
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2020-<YOUR_ACCOUNT_0000_HERE>.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2021-<YOUR_ACCOUNT_0000_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2022-<YOUR_ACCOUNT_0000_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-<YOUR_ACCOUNT_0000_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-<YOUR_ACCOUNT_0000_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
 
-# 3021-6825
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-3021-6825.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-3021-6825.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-3021-6825.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-3021-6825.csv
+python "${BASE_DIR}"/tools/cleanup_kiwoom_data.py --file "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0000_HERE>.csv
 
-# 3480-4607
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-3480-4607.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-3480-4607.csv
-cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-3480-4607.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-3480-4607.csv
 
+# <YOUR_ACCOUNT_0001_HERE>
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-<YOUR_ACCOUNT_0001_HERE>.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0001_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-<YOUR_ACCOUNT_0001_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0001_HERE>.csv
+
+python "${BASE_DIR}"/tools/cleanup_kiwoom_data.py --file "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0001_HERE>.csv
+
+# <YOUR_ACCOUNT_0002_HERE>
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2023-<YOUR_ACCOUNT_0002_HERE>.csv > "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0002_HERE>.csv
+cat "${BASE_DIR}"/data/kiwoom-exported-transactions/year-2024-<YOUR_ACCOUNT_0002_HERE>.csv >> "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0002_HERE>.csv
+
+python "${BASE_DIR}"/tools/cleanup_kiwoom_data.py --file "${BASE_DIR}"/data/kiwoom-exported-transactions/latest-<YOUR_ACCOUNT_0002_HERE>.csv
