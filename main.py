1335d11673825905f23bb2a2495067be854eb73f
Zhang-hoon Dennis Oh <zhanghoondennisoh@gmail.com>
2024-11-16 01:08:28 +0900

    feat: add command line interface
    - Add command line interface using click.
    - Create `create` command to import Kiwoom transactions.
    - Create `export` command to export to Yahoo Finance.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 This module interprets command from a user, the do the job.
@@ -7,6 +8,7 @@ import argparse
 import sys
 import yaml
 
+import click
 from loguru import logger
 
 import investing_dot_com_text_exporter
@@ -55,45 +57,7 @@ def build_global_config(global_config_filepath: str) -> dict:
 
 
 def do_main_thing_with_args(args):
-    if args.global_config is None:
-        logger.error('Global configuration filepath must be provided. Use --global_config.')
-        sys.exit(-1)
-
-    global_config_filepath = args.global_config
-    global_config = build_global_config(global_config_filepath)
-
-    if global_config is None:
-        logger.error('Global configuration is malformed.')
-        sys.exit(-1)
-
-    flag_read_input_from_kiwoom = False
-    if args.kiwoom_config is not None:
-        flag_read_input_from_kiwoom = True
-
-    if not flag_read_input_from_kiwoom:
-        logger.error('Currently only reading files from Kiwoom Securities is supported.')
-        sys.exit(-1)
-
-    if flag_read_input_from_kiwoom:
-        kiwoom_config_filepath = args.kiwoom_config
-        list_of_simple_transactions = None
-
-        try:
-            list_of_simple_transactions = kiwoom_text_importer.build_list_of_simple_transactions(kiwoom_config_filepath)
-            if not list_of_simple_transactions:
-                sys.exit(-1)
-        except IOError as e:
-            logger.error(f'IOError: {e}')
-            logger.error('Input filepath was: %s' % kiwoom_config_filepath)
-            sys.exit(-1)
-
-        do_mariadb_transaction_export(global_config, list_of_simple_transactions)
-
-        portfolio = build_portfolio(list_of_simple_transactions)
-
-        do_investing_dot_com_portfolio_export(portfolio)
-
-    # do_yahoo_finance_web_export(portfolio)
+    pass
 
 
 def do_yahoo_finance_web_export(portfolio):
@@ -109,17 +73,91 @@ def do_yahoo_finance_web_export(portfolio):
     return True
 
 
-def main():
-    parser = argparse.ArgumentParser(description='This tool manages financial transactions and portfolios')
+@click.group()
+def cli():
+    # This is the base command group, providing a central entry point for the tool. Intentionally, one passes.
+    """
+    This program manages financial transactions.
+    """
+    pass
 
-    parser.add_argument('--global_config', type=str,
-                        help='Global configuration filepath')
-    parser.add_argument('--kiwoom_config', type=str,
-                        help='Kiwoom configuration filepath')
-    args = parser.parse_args()
 
-    do_main_thing_with_args(args)
+@cli.group()
+def create():
+    """
+    This command creates an object.
+    """
+    pass
+
+
+@create.command()
+@click.option('--global-config', required=True, help='Global configuration filepath.')
+@click.option('--kiwoom-config', required=True, help='Kiwoom configuration filepath.')
+def kiwoom_transaction(global_config, kiwoom_config):
+    """
+    Import Kiwoom Securities transaction file and create transaction records in a database.
+    """
+    if global_config is None:
+        logger.error('Global configuration filepath must be provided. Use --global_config.')
+        sys.exit(-1)
+
+    global_config_filepath = global_config
+    global_config_dict = build_global_config(global_config_filepath)
+
+    if global_config_dict is None:
+        logger.error('Global configuration is malformed.')
+        sys.exit(-1)
+
+    flag_read_input_from_kiwoom = False
+    if kiwoom_config is not None:
+        flag_read_input_from_kiwoom = True
+
+    if not flag_read_input_from_kiwoom:
+        logger.error('Currently only reading files from Kiwoom Securities is supported.')
+        sys.exit(-1)
+
+    if flag_read_input_from_kiwoom:
+        kiwoom_config_filepath = kiwoom_config
+        list_of_simple_transactions = None
+
+        try:
+            list_of_simple_transactions = kiwoom_text_importer.build_list_of_simple_transactions(kiwoom_config_filepath)
+            if not list_of_simple_transactions:
+                sys.exit(-1)
+        except IOError as e:
+            logger.error(f'IOError: {e}')
+            logger.error('Input filepath was: %s' % kiwoom_config_filepath)
+            sys.exit(-1)
+
+        do_mariadb_transaction_export(global_config_dict, list_of_simple_transactions)
+
+        portfolio = build_portfolio(list_of_simple_transactions)
+
+        do_investing_dot_com_portfolio_export(portfolio)
+
+
+@cli.group()
+def export():
+    """
+    This command exports data to an external service.
+    """
+    pass
+
+
+@export.command()
+def yahoo_finance():
+    # @FIXME(dennis.oh) Instead of None, read portfolio.
+    portfolio = None
+    do_yahoo_finance_web_export(portfolio)
+
+
+@cli.group()
+def get():
+    """
+    This command gets and displays data.
+    """
+    pass
 
 
 if __name__ == '__main__':
-    main()
+    cli()
